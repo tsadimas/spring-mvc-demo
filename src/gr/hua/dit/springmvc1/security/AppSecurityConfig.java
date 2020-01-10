e090aa1d5dcaa9c87ad8a448db32cd8279af0617
Anargyros Tsadimas <tsadimas@hua.gr>
2020-01-10 12:47:50 +0200

    configure security for /api and form login

--- src/gr/hua/dit/springmvc1/security/AppSecurityConfig.java
+++ src/gr/hua/dit/springmvc1/security/AppSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,8 +24,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
 				.usersByUsernameQuery("select username,password, enabled from user where username=?")
@@ -32,27 +33,43 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.loginProcessingUrl("/authUser").permitAll().and().logout().permitAll().and().exceptionHandling()
-				.accessDeniedPage("/403");
-
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**");
-
-		web.ignoring().antMatchers("/api/**");
-
-	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
 	}
+	
+	 @Configuration
+	    @Order(1)
+	    public static class ApiWebSecurityConfig extends WebSecurityConfigurerAdapter{
+	        @Override
+	        protected void configure(HttpSecurity http) throws Exception {
+	        	  http.csrf().disable()
+	              .antMatcher("/api/**")
+	              .authorizeRequests()
+	                  .anyRequest().authenticated()
+	                  .and()
+	              .httpBasic();
+	        }
+	    }
+
+	    @Configuration
+	    @Order(2)
+	    public static class FormWebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	        @Override
+	        public void configure(WebSecurity web) throws Exception {
+	    		web.ignoring().antMatchers("/resources/**");
+	        }
+
+	        @Override
+	        protected void configure(HttpSecurity http) throws Exception {
+	        	http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
+				.loginProcessingUrl("/authUser").permitAll().and().logout().permitAll().and().exceptionHandling()
+				.accessDeniedPage("/403");
+	        }
+	    }
 	
 	
 }
